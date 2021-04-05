4446eb03fd961442a70713b512ae5d59f5b0baad
Eli Almario <ee.almario@gmail.com>
2021-04-05 20:08:39 +0800

    npm install uuid | Create MeetUpItem component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script>
+	import { v4 as uuidv4 } from 'uuid';
 	import Header from './components/Header.svelte';
+	import MeetUpItem from './components/MeetUpItem.svelte';
+
+	const meetups = [
+		{
+			id: uuidv4(),
+			imgUrl: 'http://via.placeholder.com/300',
+			name: 'Coding Bootcamp',
+			subtitle: 'Learn to code in 4 hours',
+			description: 'In this meetup, we will have experts that would teach you how to code',
+			address: '724 Kayla Branch' ,
+			contactPerson: 'Johanna Larson',
+			contactEmail: 'jlarson@code.com'
+		},
+		{
+			id: uuidv4(),
+			imgUrl: 'http://via.placeholder.com/300',
+			name: 'Coffee Workshop',
+			subtitle: 'Manual Brewing Methods',
+			description: 'Learn the different types of brewing methods and create the perfect cup even at home! Workshop is from 3pm - 5pm on Zoom and FB Live.',
+			address: 'Apt. 818 47298 Kertzmann Summit Cubao',
+			contactPerson: 'Jennie Pfeffer',
+			contactEmail: 'jennie,pfeffer@coffeeworks.com'
+		},
+	];
+
+	console.log(meetups);
 </script>
 
 <Header />
+{#each meetups as meetup}
+	<MeetUpItem meetup={meetup} />
+{/each}
---
+++ src/components/MeetUpItem.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let meetup;
+</script>
+
+<div class="card">
+  <figure class="card-header">
+    <img src={meetup.imgUrl} alt="Meetup Item">
+  </figure>
+  <div class="card-body">
+    <h5 class="name">{meetup.name}</h5>
+    <h6>{meetup.subtitle}</h6>
+    <p class="poster">Posted by:
+      <a href="mailto:{meetup.contactEmail}">
+        {meetup.contactPerson}
+      </a> 
+    </p>
+    <div class="divider"></div>
+    <p class="description">{meetup.description}</p>
+    <p class="address">{meetup.address}</p>
+  </div>
+  <div class="card-footer">
+    <button>See More</button>
+    <button>Add to Favorites</button>
+  </div>
+</div>
