f4a5817cd7e5a0437fe09a01384e7b9f34b26ee0
Eli Almario <ee.almario@gmail.com>
2021-04-30 08:42:23 +0800

    Create SearchInput component | Adjust home page styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { v4 as uuidv4 } from 'uuid';
-	import Header from './components/Header.svelte';
+	import SearchInput from './components/SearchInput.svelte';
 	import MeetUpGrid from './components/MeetUpGrid.svelte';
 	import TextInput from './components/TextInput.svelte';
 	import Button from './components/Button.svelte';
@@ -48,10 +48,61 @@
 <style lang="scss">
 	main {
 		padding: 1rem;
+		height: 100vh;
+		width: 100vw;
+	}
+
+	.explore-container {
+		position: relative;
+		margin-bottom: 2rem;
+	}
+
+	.search-input {
+		position: absolute;
+		bottom: -1.25rem;
+		left: 5%;
+		width: 100%;
+		z-index: 3;
+	}
+
+	.events-bg {
+		background: url('/images/explore-photo.jpg') no-repeat bottom center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		height: 50vh;
+		width: 100%;
+	}
+
+	.overlay {
+		background: rgba(0,0,0,.6);
+		height: 50vh;
+		width: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	.explore-text {
+		color: #fcfcfc;
+		font-size: 2rem;
+		letter-spacing: 0.25;
+		position: absolute;
+		top: 2rem;
+		left: 1.25rem;
 	}
 </style>
 
 <!-- <Header /> -->
+<div class="explore-container">
+	<div class="search-input">
+		<SearchInput />
+	</div>
+	<div class="overlay"></div>
+	<h3 class="explore-text">Explore</h3>
+	<div class="events-bg"></div>
+</div>
 <main>
 	<!-- <form on:submit|preventDefault={addMeetUp}>
 		<TextInput 
---
+++ src/components/SearchInput.svelte
@@ -0,0 +1,21 @@
+<script></script>
+
+<style lang="scss">
+  .form-control {
+    width: 90%;
+  }
+
+  input {
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0,0,0,0.2);
+    font-size: 0.75rem;
+    font-weight: bold !important;
+    padding: 1rem;
+    width: 100%;
+  }
+</style>
+
+<div class="form-control">
+  <input type="text" placeholder="Looking for something specific?"/>
+</div>
