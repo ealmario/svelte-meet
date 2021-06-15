91a459dbcd9461fe9a14a34159e99d09c90a5c07
Eli Almario <ee.almario@gmail.com>
2021-06-15 08:52:31 +0800

    Create meetup store | Refactor code to use custom meetup store

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,21 @@
 <script>
-	import { v4 as uuidv4 } from 'uuid';
+	import meetups from './stores/meetup-store';
 	import SearchInput from './components/SearchInput.svelte';
 	import MeetUpGrid from './components/MeetUpGrid.svelte';
 	import EditMeetUp from './components/EditMeetUp.svelte';
 	import FloatingActionButton from './components/FloatingActionButton.svelte';
-
-	let meetups = [
-    {
-      id: uuidv4(),
-      imgUrl: 'https://images.summitmedia-digital.com/esquiremagph/images/2019/03/13/WeWork_MAIN_Mar2019.jpg',
-      name: 'Coding Bootcamp',
-      schedule: 'May 3, 2021',
-      description: 'In this meetup, we will have experts that would teach you how to code',
-      address: '724 Kayla Branch' ,
-      contactPerson: 'Johanna Larson',
-			contactEmail: 'jlarson@code.com',
-			isFavorite: false
-    },
-    {
-      id: uuidv4(),
-      imgUrl: 'https://bigseventravel.com/wp-content/uploads/2019/09/Blocleaf-Cafe%CC%81.jpeg',
-      name: 'Coffee Workshop',
-      schedule: 'June 20, 2021',
-      description: 'Learn the different types of brewing methods and create the perfect cup even at home! Workshop is from 3pm - 5pm on Zoom and FB Live.',
-      address: 'Apt. 818 47298 Kertzmann Summit Cubao',
-      contactPerson: 'Jennie Pfeffer',
-			contactEmail: 'jennie,pfeffer@coffeeworks.com',
-			isFavorite: false
-    },
-	];
 
 	let editMode = false;
 	
 	function toggleFavorite(event) {
 		// Extract ID from event detail
 		const id = event.detail;
-		// Copy meetup array before updating
-		const updatedMeetup = {...meetups.find(meetup => meetup.id === id)};
-		// Toggle isFavorite
-		updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-		// Get the index of the meetup
-		const meetupIndex = meetups.findIndex(meetup => meetup.id === id);
-		// Create a copy of meetups and update the specific meetup
-		const updatedMeetups = [...meetups];
-		updatedMeetups[meetupIndex] = updatedMeetup;
-		meetups = updatedMeetups;
+		meetups.toggleFavorite(id);
 	}
 
 	function addMeetUp(event) {
-		const meetup = event.detail;
-		meetups = [...meetups, meetup];
+		const meetupData = event.detail;
+		meetups.addMeetUp(meetupData);
 		editMode = false;
 		console.log(meetups);
 	}
@@ -126,7 +92,7 @@
 
 <main>
 	<MeetUpGrid 
-		meetups={meetups}
+		meetups={$meetups}
 		on:togglefavorite={toggleFavorite}
 	/>
 	<FloatingActionButton on:click={()=> (editMode = true)}/>
---
+++ src/stores/meetup-store.js
@@ -0,0 +1,61 @@
+import { writable } from 'svelte/store';
+import { v4 as uuidv4 } from 'uuid';
+
+const meetups = writable([
+  {
+    id: uuidv4(),
+    imgUrl: 'https://images.summitmedia-digital.com/esquiremagph/images/2019/03/13/WeWork_MAIN_Mar2019.jpg',
+    name: 'Coding Bootcamp',
+    schedule: 'May 3, 2021',
+    description: 'In this meetup, we will have experts that would teach you how to code',
+    address: '724 Kayla Branch' ,
+    contactPerson: 'Johanna Larson',
+    contactEmail: 'jlarson@code.com',
+    isFavorite: false
+  },
+  {
+    id: uuidv4(),
+    imgUrl: 'https://bigseventravel.com/wp-content/uploads/2019/09/Blocleaf-Cafe%CC%81.jpeg',
+    name: 'Coffee Workshop',
+    schedule: 'June 20, 2021',
+    description: 'Learn the different types of brewing methods and create the perfect cup even at home! Workshop is from 3pm - 5pm on Zoom and FB Live.',
+    address: 'Apt. 818 47298 Kertzmann Summit Cubao',
+    contactPerson: 'Jennie Pfeffer',
+    contactEmail: 'jennie,pfeffer@coffeeworks.com',
+    isFavorite: false
+  },
+]);
+
+function addMeetUp(meetupData) {
+  const newMeetUp = {
+    ...meetupData,
+    id: uuidv4(),
+    isFavorite: false
+  }
+  meetups.update( items => {
+    return [newMeetUp, ...items];
+  })
+}
+
+function toggleFavorite (id) {
+  meetups.update( items => {
+    // Copy meetup array before updating
+    const updatedMeetup = {...items.find(meetup => meetup.id === id)};
+    // Toggle isFavorite
+    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+    // Get the index of the meetup
+    const meetupIndex = items.findIndex(meetup => meetup.id === id);
+    // Create a copy of meetups and update the specific meetup
+    const updatedMeetups = [...items];
+    updatedMeetups[meetupIndex] = updatedMeetup;
+    return updatedMeetups;
+  })
+}
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  addMeetUp,
+  toggleFavorite
+};
+
+export default customMeetupsStore;
