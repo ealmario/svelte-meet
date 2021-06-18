2979ab9d64d6ef5b953fe734fb1a5c0f7b160ae5
Eli Almario <ee.almario@gmail.com>
2021-06-18 08:44:57 +0800

    Create base MeetUpDetail component

---
+++ src/components/MeetUpDetail.svelte
@@ -0,0 +1,67 @@
+<script>
+  import { onDestroy } from 'svelte';
+  import meetups from './../stores/meetup-store';
+  import Button from './Button.svelte';
+  export let id;
+
+  let selectedMeetup;
+
+  const unsubscribe = meetups.subscribe( items => {
+    selectedMeetup = items.find( item => item.id === id);
+  })
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+</script>
+
+<style lang="scss">
+  .mdetail_container {
+    background: #fff;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .mdetail_navigation {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.5rem;
+    width: 100%;
+  }
+
+  .mdetail_content {
+    background: #fff;
+    border-radius: 24px 24px 0 0;
+  }
+</style>
+
+<div class="mdetail_container">
+  <div class="mdetail_image">
+    <header class="mdetail_navigation">
+      Back Button Icon
+    </header>
+  </div>
+
+  <div class="mdetail_content">
+    <div class="mdetail_title_content">
+      <h2 class="mdetail_title_text">{selectedMeetup.name}</h2>
+      Favorite
+      Share
+    </div>
+
+    <!-- Scrollable Content -->
+    <div class="mdetail_list">
+      <!-- Event Details Here -->
+      Date, time, etc.
+    </div>
+
+    <footer class="mdetail_footer">
+      <Button
+        type="button"
+        text="Get Tickets"
+        variant="solid-primary"
+      />
+    </footer>
+  </div>
+</div>
